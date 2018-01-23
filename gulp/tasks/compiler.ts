bcded5381dc95792335e9689aff39251f42ad3db
Ethan Ferrari <github@ethanferrari.com>
2018-01-22 23:39:13 -0600

    gulp sourcemaps for server -- thanks Kurt Preston

--- gulp/tasks/compiler.ts
+++ gulp/tasks/compiler.ts
@@ -1,6 +1,7 @@
 import {WatchEvent} from 'gulp';
 import * as ts from 'gulp-typescript';
 import {TaskFactory} from '../taskFactory';
+import * as sourcemaps from 'gulp-sourcemaps';
 
 let tsProject: ts.Project;
 
@@ -9,7 +10,12 @@ export const compileServer: TaskFactory = (gulp, {rootPath, buildDir}) => () => 
     ts.createProject(`${rootPath}/src/server/tsconfig.json`)
   );
   return gulp.src(`${rootPath}/src/server/**/*.ts`)
+    .pipe(sourcemaps.init())
     .pipe(project())
+    .pipe(sourcemaps.write('.', {
+      includeContent: false,
+      sourceRoot: './'
+    }))
     .pipe(gulp.dest(`${rootPath}/${buildDir}/server`));
 };
 
