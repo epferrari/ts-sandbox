d306d87f1359cb8d86083cba5357f240f6983033
Ethan Ferrari <github@ethanferrari.com>
2018-01-22 23:48:19 -0600

    add command line args to nodemon

--- gulp/tasks/runDevServer.ts
+++ gulp/tasks/runDevServer.ts
@@ -4,7 +4,14 @@ import {TaskFactory} from '../taskFactory';
 const devServer: TaskFactory = (gulp, {registerChildProcess, rootPath, buildDir}) => (done) => {
   const child: ChildProcess = spawn(
     `${rootPath}/node_modules/.bin/nodemon`,
-    [`-w`, `./${buildDir}/server`, `./${buildDir}/server`],
+    [
+      `-w`,
+      `./${buildDir}/server`,
+      `./${buildDir}/server`,
+      `--trace-warnings`,
+      `-r`,
+      `source-map-support/register`
+    ],
     {
       cwd: rootPath,
       env: {...process.env, NODE_ENV: 'development'}
