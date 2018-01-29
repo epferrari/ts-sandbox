e1af4fe7b09d999df1b36cae946bef6ae9be686d
Ethan Ferrari <github@ethanferrari.com>
2018-01-28 21:17:27 -0600

    remove a blank line to test precommit hook

--- gulp/tasks/clean.ts
+++ gulp/tasks/clean.ts
@@ -1,7 +1,6 @@
 import {TaskFactory} from '../taskFactory';
 import * as del from 'del';
 
-
 export const cleanServer: TaskFactory<void> = (gulp, {rootPath, buildDir}) => () => {
   return del([`${rootPath}/${buildDir}/server`]);
 };
