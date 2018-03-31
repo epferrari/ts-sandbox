b4b50ad9211b5ba41b70ea9c7ab2efb7db2d8f38
Ethan Ferrari <github@ethanferrari.com>
2018-03-30 23:42:53 -0500

    add helmet to express middleware stack

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as helmet from 'helmet';
 import {logging} from './middleware/logging';
 import {errorHandler} from './middleware/errorHandler';
 import {api} from './routes/api';
@@ -6,6 +7,7 @@ import {api} from './routes/api';
 const app = express();
 
 app
+  .use(helmet())
   .use(logging)
   .use('/api', api)
   .get('*', (req: express.Request, res: express.Response) => res.json({
